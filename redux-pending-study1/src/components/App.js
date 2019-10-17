505e514988d5c191d1eb717a53a2e6e8c1455241
phantom05 <monster2jy@gmail.com>
2019-10-17 18:43:18 +0900

    스캔앱 리액트 포팅 해보기

--- redux-pending-study1/src/components/App.js
+++ redux-pending-study1/src/components/App.js
@@ -2,8 +2,30 @@ import React, { Component } from 'react';
 import {Provider} from 'react-redux';
 import CounterContainer from 'containers/CounterContainer';
 import store from 'store';
+import {QWebChannel} from 'qwebchannel';
+import {ScapApp} from 'lib/api/ScapAppQWebchannel';
+
+import {w3cwebsocket as W3CWebSocket} from 'websocket';
+
+
+const testMode = true;
+const serverHost = testMode ? 'ws://127.0.0.1:8082':'ws://127.0.0.1:5501';
+const client = new W3CWebSocket(serverHost);
 
 class App extends Component {
+  componentDidMount(){
+    client.onopen =() =>{
+      console.log('WebSocket Client Connected');
+      if(testMode){ 
+        ScapApp(client);
+      }else{  
+        new QWebChannel(client, App); 
+      }
+    }
+    client.onmessage = (message) =>{
+      console.log(message.data);
+    }
+  }
   render() {
     return (
       <div>
---
+++ redux-pending-study1/src/lib/api/ScapAppQWebchannel.js
@@ -0,0 +1,8 @@
+
+
+
+export const ScapApp = (ws) =>{
+  ws.send(JSON.stringify({'hello':"world1"}))
+}
+
+// export default ScapApp
