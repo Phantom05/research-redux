bab2550ac9d659967350a2d388ab8dd2ff90e4c8
phantom05 <monster2jy@gmail.com>
2019-11-11 08:27:20 +0900

    라면 이미지 업로드

--- howto-client/src/components/base/images/index.js
+++ howto-client/src/components/base/images/index.js
@@ -1,8 +1,9 @@
 import ai_test from 'static/images/ai_test.jpg';
 import cook_v1 from 'static/images/전복비빔밥.png';
-
+import cook_v2 from 'static/images/라면.jpg';
 
 export {
   ai_test,
-  cook_v1
+  cook_v1,
+  cook_v2
 }
--- howto-client/src/containers/home/HomeContainer.js
+++ howto-client/src/containers/home/HomeContainer.js
@@ -4,7 +4,7 @@ import PlainTemplate from 'components/base/template/PlainTemplate';
 import MainHeader from 'components/common/header/MainHeader';
 import MainNavigation from 'components/common/navigation/MainNavigation';
 import { LinePost } from 'components/common/post';
-import { ai_test,cook_v1 } from 'components/base/images';
+import { cook_v2,cook_v1 } from 'components/base/images';
 
 class HomeContainer extends Component {
   render() {
@@ -23,7 +23,7 @@ class HomeContainer extends Component {
       },
       {
         id: 1,
-        image: <img src={ai_test} alt={ai_test} />,
+        image: <img src={cook_v2} alt={cook_v2} />,
         title: "라면은 어떻게해야 잘 끓일수 있는가?",
         subTitle: "라면wehwehweh을 잘 끓이기 위해선 물의 양이 매우 중요합니다. 1. 물의 양을 조절할떈 비커를 사용해서 사용한다.zzzz 라면을 잘 끓이기 위해선 물의 양이 매우 중요합니다. 1. 물의 양을 조절할떈 비커를 사용해서 사용한다",
         date: "2019-11-11 / 07:53",
